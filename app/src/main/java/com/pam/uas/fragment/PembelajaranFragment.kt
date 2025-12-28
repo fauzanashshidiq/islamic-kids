6100af4e08385c0777ce1ca4c7bfe750a63932d4
fauzanashshidiq <muhzeen07@gmail.com>
2025-12-28 20:27:28 +0700

    feat(pembelajaran): add fun preloadpembelajaran on fragment

--- app/src/main/java/com/pam/uas/fragment/PembelajaranFragment.kt
+++ app/src/main/java/com/pam/uas/fragment/PembelajaranFragment.kt
@@ -6,15 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.pam.uas.DetailPembelajaranActivity
 import com.pam.uas.databinding.FragmentPembelajaranBinding
 import com.pam.uas.ui.PembelajaranMenuAdapter
+import com.pam.uas.viewmodel.PembelajaranViewModel
+import kotlin.getValue
 
 class PembelajaranFragment : Fragment() {
 
  private var _binding: FragmentPembelajaranBinding? = null
  private val binding get() = _binding!!
+
+ private val viewModel: PembelajaranViewModel by viewModels()
 
  override fun onCreateView(
   inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +31,8 @@ class PembelajaranFragment : Fragment() {
 
  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
   super.onViewCreated(view, savedInstanceState)
+
+  viewModel.preloadPembelajaran()
 
   // Setup Adapter
   val adapter = PembelajaranMenuAdapter { menu ->
