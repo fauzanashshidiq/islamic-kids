e16b1ffd6927f28b622be11ad96b3d79011f8e79
rhrdianbaihaqi <haqistream@gmail.com>
2025-12-29 15:34:29 +0700

    Update halaman splashscreen n welcome

---
+++ app/src/main/java/com/pam/uas/SplahScreen.kt
@@ -0,0 +1,30 @@
+package com.pam.uas
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplahScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splah_screen)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Tampilkan splash selama 5 detik, lalu pindah ke Welcome
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, Welcome::class.java))
+            finish()
+        }, 5000) // 5000 ms = 5 detik
+    }
+}
---
+++ app/src/main/java/com/pam/uas/Welcome.kt
@@ -0,0 +1,31 @@
+package com.pam.uas
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.AppCompatButton
+
+class Welcome : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // ===== IMPLEMENTASI BUTTON MULAI =====
+        val btnMulai = findViewById<AppCompatButton>(R.id.btn_mulai)
+        btnMulai.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
